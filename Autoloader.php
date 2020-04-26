7262812fd57f2c2bcb37ee303eee95ea1a320ad9
Lukasz Nowicki <lukasz.nowicki@post.pl>
2020-04-26 19:23:37 +0200

    Use PHP 7.4 syntax Add namespace/directory arguments for the constructor New README.md with new examples Somme doc tweaks

--- Autoloader.php
+++ Autoloader.php
@@ -1,171 +1,181 @@
 <?php
 /**
- * This is the main file of the plugin.
+ * Now it uses PHP 7.4 standards
  *
- * @package   Phylax
- * @version   0.1.2
+ * @version   0.2.1
  * @since     0.1.0
- * @author    Łukasz Nowicki <lukasz.nowicki@post.pl>
- * @copyright phylax.pl Łukasz Nowicki
+ * @author    Łukasz Nowicki <kontakt@phylax.pl>
+ * @copyright Łukasz Nowicki
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU GPLv3 or later
  * @link      https://github.com/lukasznowicki/psr-autoloader
  */
 
 namespace Phylax;
 
+use Exception;
+use const DIRECTORY_SEPARATOR;
+
 /**
  * Class Autoloader
  *
- * @package Phylax
+ * Use this class to handle auto loading classes, traits, implementations and abstracts.
+ * You may declare namespace and proper directory in the constructor (usually use this
+ * feature when you got only one) or by using register_handler and addNamespace methods.
  */
 class Autoloader {
 
-
 	/**
-	 * @var array List of registered namespaces
+	 * @var array
 	 */
-	protected $namespaces = [];
+	protected array $namespaces = [];
 
 	/**
-	 * This method will register Class Autoload handler. As it is vital for
-	 * the project, this method always returns true or exits on failure.
+	 * Autoloader constructor. You may assign namespace and directory in the constructor. If you do, the register
+	 * handler method will be invoked automatically
 	 *
-	 * @return bool Always returns True, exiting on error
+	 * @param string|null $namespace
+	 * @param string|null $directory
+	 * @param bool $exitOnFail
 	 */
-	public function registerHandler(): bool {
-		try {
-			spl_autoload_register( [ $this, 'classLoader' ] );
-		} catch ( \Exception $exception ) {
-			exit( 'Example Plugin error, cannot register autoloader this is unrecoverable.' );
+	public function __construct( ?string $namespace = null, ?string $directory = null, bool $exitOnFail = true ) {
+		if ( ! is_null( $namespace ) && ! is_null( $directory ) ) {
+			$this->registerHandler( $exitOnFail );
+			$this->addNamespace( $namespace, $directory );
 		}
-
-		return TRUE;
 	}
 
 	/**
-	 * Add namespace with associated directory to the loader
+	 * @param bool $exitOnFail
 	 *
-	 * @param string $namespace Namespace entry point
-	 * @param string $directory Directory entry point for given namespace
-	 * @param bool   $prepend   Prepend this entry (True) or append at the end
-	 *                          (False, default)
-	 *
-	 * @return bool Always returns true
+	 * @return bool
 	 */
-	public function addNamespace( string $namespace, string $directory, bool $prepend = FALSE ): bool {
+	public function registerHandler( bool $exitOnFail = true ): bool {
+		try {
+			spl_autoload_register( [
+				$this,
+				'classLoader',
+			] );
+		} catch ( Exception $exception ) {
+			if ( $exitOnFail ) {
+				exit;
+			}
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param string $namespace
+	 * @param string $directory
+	 * @param bool $prepend
+	 *
+	 * @return bool
+	 */
+	public function addNamespace( string $namespace, string $directory, bool $prepend = false ): bool {
 		$namespace = $this->prepareNamespace( $namespace );
 		$directory = $this->prepareDirectory( $directory );
-		if ( FALSE === isset( $this->namespaces[ $namespace ] ) ) {
+		if ( ! isset( $this->namespaces[ $namespace ] ) ) {
 			$this->namespaces[ $namespace ] = [];
 		}
-		if ( FALSE === $prepend ) {
-			array_push( $this->namespaces[ $namespace ], $directory );
-		} else {
+		if ( $prepend ) {
 			array_unshift( $this->namespaces[ $namespace ], $directory );
+		} else {
+			array_push( $this->namespaces[ $namespace ], $directory );
 		}
 
-		return TRUE;
+		return true;
 	}
 
 	/**
-	 * Make sure, that given namespace ends up with a slash
+	 * @param string $namespace
 	 *
-	 * @param string $namespace Desired namespace
-	 *
-	 * @return string Proper namespace
+	 * @return string
 	 */
-	private function prepareNamespace( string $namespace ): string {
-		$namespace = trim( $namespace, '\\' ) . '\\';
-
-		return $namespace;
+	protected function prepareNamespace( string $namespace ): string {
+		return $this->prepareString( $namespace, '\\' );
 	}
 
 	/**
-	 * Make sure that directory uses proper directory separator, as defined by
-	 * the PHP on current machine and ends up with one
+	 * @param string $string
+	 * @param string $inUse
 	 *
-	 * @param string $directory Desired directory
-	 *
-	 * @return string Proper directory
+	 * @return string
 	 */
-	private function prepareDirectory( string $directory ): string {
-		$directory = str_replace( [
+	protected function prepareString( string $string, string $inUse ): string {
+		$string = str_replace( [
 			'\\',
 			'/',
-		], \DIRECTORY_SEPARATOR, $directory );
-		$directory = rtrim( $directory, \DIRECTORY_SEPARATOR ) . \DIRECTORY_SEPARATOR;
+		], $inUse, $string );
+		$string = rtrim( $string, $inUse ) . $inUse;
 
-		return $directory;
+		return $string;
 	}
 
 	/**
-	 * Traverse the class path against the directory structure, if find
-	 * suitable entry, calls to check files
+	 * @param string $directory
 	 *
-	 * @see \Phylax\Autoloader::checkMappedFile()
+	 * @return string
+	 */
+	protected function prepareDirectory( string $directory ): string {
+		return $this->prepareString( $directory, DIRECTORY_SEPARATOR );
+	}
+
+	/**
+	 * @param string $class
 	 *
-	 * @param string $class Desired class, this one is given by the PHP
-	 *
-	 * @return null|string Returns string on success or null on failure
+	 * @return string|null
 	 */
 	public function classLoader( string $class ): ?string {
 		$prefix = $class;
-		while ( FALSE !== $position = strrpos( $prefix, '\\' ) ) {
-			$prefix        = substr( $class, 0, $position + 1 );
-			$relativeClass = substr( $class, $position + 1 );
-			$mappedFile    = $this->checkMappedFile( $prefix, $relativeClass );
-			if ( TRUE === $mappedFile ) {
-				return $mappedFile;
+		while ( false !== $position = strrpos( $prefix, '\\' ) ) {
+			$prefix  = substr( $class, 0, $position + 1 );
+			$relCls  = substr( $class, $position + 1 );
+			$mapFile = $this->checkMappedFile( $prefix, $relCls );
+			if ( $mapFile ) {
+				return $mapFile;
 			}
 			$prefix = rtrim( $prefix, '\\' );
 		}
 
-		return NULL;
+		return null;
 	}
 
 	/**
-	 * When a proper Namespace is found, let's traverse assigned directories
-	 * to find a file and try to call it
+	 * @param string $namespace
+	 * @param string $relClass
 	 *
-	 * @see \Phylax\Autoloader::classLoader()
-	 * @see \Phylax\Autoloader::callFile()
-	 *
-	 * @param string $namespace     Desired namespace
-	 * @param string $relativeClass Relative class name
-	 *
-	 * @return null|string String on success or null on failure
+	 * @return string|null
 	 */
-	protected function checkMappedFile( string $namespace, string $relativeClass ): ?string {
-		if ( FALSE === isset( $this->namespaces[ $namespace ] ) ) {
-			return NULL;
+	protected function checkMappedFile( string $namespace, string $relClass ): ?string {
+		if ( false === isset( $this->namespaces[ $namespace ] ) ) {
+			return null;
 		}
-		foreach ( $this->namespaces[ $namespace ] as $baseDirectory ) {
-			$filePath = $baseDirectory . str_replace( '\\', \DIRECTORY_SEPARATOR, $relativeClass ) . '.php';
-			if ( TRUE === $this->callFile( $filePath ) ) {
+		foreach ( $this->namespaces[ $namespace ] as $baseDir ) {
+			$filePath = $baseDir . str_replace( '\\', DIRECTORY_SEPARATOR, $relClass ) . '.php';
+			if ( $this->callFile( $filePath ) ) {
 				return $filePath;
 			}
 		}
 
-		return NULL;
+		return null;
 	}
 
 	/**
-	 * If given file is readable, then load it once
+	 * @param string $filePath
 	 *
-	 * @see \Phylax\Autoloader::checkMappedFile()
-	 *
-	 * @param string $filePath Class file path
-	 *
-	 * @return bool True on success, false otherwise
+	 * @return bool
+	 * @noinspection PhpIncludeInspection
 	 */
 	protected function callFile( string $filePath ): bool {
-		if ( TRUE === is_readable( $filePath ) ) {
+		if ( is_readable( $filePath ) && ! is_dir( $filePath ) ) {
 			require_once $filePath;
 
-			return TRUE;
+			return true;
 		}
 
-		return FALSE;
+		return false;
 	}
 
 }
--- README.md
+++ README.md
@@ -1,11 +1,31 @@
 # PSR Autoloader
-PSR Autoloader to use with my projects
+I wrote small PSR-4 Autoloader to use with my projects. Feel free to use it in yours.
 
-Use:
+#####Warning: This class uses PHP version 7.4 syntax
 
+Some example codes:
+
+```php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+$theAutoloader = new \Phylax\Autoloader();
+$theAutoloader->registerHandler();
+$theAutoloader->addNamespace( 'Phylax', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' );
+$theAutoloader->addNamespace( 'OtherStuff', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'OtherStuff' );
+# etc.
 ```
-require 'Phylax\Autoloader';
-$my_autoloader = new \Phylax\Autoloader();
-$my_atuoloader->registerHandler();
-$my_autoloader->addNamespace( 'My\Class\Namespace', __DIR__ );
+
+Or:
+```php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+new \Phylax\Autoloader( 'OtherStuff', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'OtherStuff' );
+```
+
+Of course it is always good to check, if class is/is not available. It's possible, e.g. in a WordPress environment where you can find many plugins and themes.
+For example:
+
+```php
+if ( ! class_exists( 'Phylax\Autoloader' ) ) {
+	require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+}
+new \Phylax\Autoloader( 'OtherStuff', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'OtherStuff' );
 ```
