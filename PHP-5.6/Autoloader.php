2aa57fcfdd4f1c8e9068000db6f6c468e623bc04
Lukasz Nowicki <lukasz.nowicki@post.pl>
2020-05-09 13:48:40 +0200

    Split Autoloader versions for PHP versions. Add .gitignore New texts in README.md

---
+++ PHP-5.6/Autoloader.php
@@ -0,0 +1,255 @@
+<?php
+/**
+ * Minimal requirements for this version: PHP 5.6
+ *
+ * @version   0.3.0
+ * @author    Łukasz Nowicki <kontakt@phylax.pl>
+ * @copyright Łukasz Nowicki
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU GPLv3 or later
+ * @link      https://github.com/lukasznowicki/psr-autoloader
+ */
+
+namespace Phylax;
+
+use Exception;
+use const DIRECTORY_SEPARATOR;
+
+/**
+ * Class Autoloader
+ *
+ * Use this class to handle auto loading classes, traits, implementations and abstracts.
+ * You may declare namespace and proper directory in the constructor (usually use this
+ * feature when you got only one) or by using registerHandler and addNamespace methods.
+ */
+class Autoloader {
+
+    /**
+     * The namespace/directory pairs stack. This will be used to store provided
+     * pairs.
+     *
+     * @var array
+     */
+    protected $namespaces = [];
+
+    /**
+     * Autoloader constructor. You may assign namespace/directory pair in the constructor.
+     * It is useful, if you have good PSR-4 directory structure. If you do, the register
+     * handler method will be invoked automatically.
+     * This method will use standard class methods to add the namespace/directory into
+     * the stack, so both will be sanitized as in addNamespace() method.
+     *
+     * @param string $namespace Namespace you want to set
+     * @param string $directory Directory you want to set
+     * @param bool $exitOnFail Set this to false, if you have any fallback for class loader
+     *
+     * @see Autoloader::registerHandler()
+     * @see Autoloader::addNamespace()
+     * @see Autoloader::$namespaces
+     */
+    public function __construct( $namespace = '', $directory = '', $exitOnFail = true ) {
+        if ( ( '' !== $namespace ) && ( '' !== $directory ) ) {
+            $this->registerHandler( $exitOnFail );
+            $this->addNamespace( $namespace, $directory );
+        }
+    }
+
+    /**
+     * This method will register the class auto loading handler (using PHP
+     * spl_autoload_register function). You may choose, whether you want
+     * your application to exit on fail (default) or not.
+     *
+     * @param bool $exitOnFail Set this to false, if you have any fallback for class loader
+     *
+     * @return bool
+     *
+     * @see spl_autoload_register()
+     */
+    public function registerHandler( $exitOnFail = true ) {
+        $exitOnFail = (bool) $exitOnFail;
+        try {
+            spl_autoload_register( [
+                $this,
+                'classLoader',
+            ] );
+        } catch ( Exception $exception ) {
+            if ( $exitOnFail ) {
+                exit;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Use this method to add namespace to the stack. Before addition, both namespace
+     * and directory will be sanitized to be a valid namespace and directory. Valid,
+     * does not mean - existing one. It only means proper formatting using \ for
+     * namespaces (you may, but you should not provide \ or /) and valid directory
+     * separator for current operating system, using PHP constant DIRECTORY_SEPARATOR.
+     *
+     * @param string $namespace Namespace you want to set
+     * @param string $directory Directory you want to set
+     * @param bool $prepend If you want to add the namespace/directory pair in front of the stack, just set it to true.
+     *
+     * @return bool
+     *
+     * @see Autoloader::$namespaces
+     * @see Autoloader::prepareNamespace()
+     * @see Autoloader::prepareDirectory()
+     */
+    public function addNamespace( $namespace, $directory, $prepend = false ) {
+        $namespace = (string) $this->prepareNamespace( $namespace );
+        $directory = (string) $this->prepareDirectory( $directory );
+        $prepend   = (bool) $prepend;
+        if ( ! isset( $this->namespaces[ $namespace ] ) ) {
+            $this->namespaces[ $namespace ] = [];
+        }
+        if ( $prepend ) {
+            array_unshift( $this->namespaces[ $namespace ], $directory );
+        } else {
+            array_push( $this->namespaces[ $namespace ], $directory );
+        }
+
+        return true;
+    }
+
+    /**
+     * This method will prepare namespace to be valid for PHP. It will replace
+     * / and \ characters with the valid one \. So you may use directory as
+     * the namespace as well. Anyway, you should not, because it's not pretty.
+     *
+     * @param string $namespace
+     *
+     * @return string
+     *
+     * @see Autoloader::prepareString()
+     */
+    protected function prepareNamespace( $namespace ) {
+        $namespace = (string) $namespace;
+
+        return $this->prepareString( $namespace, '\\' );
+    }
+
+    /**
+     * This method will change all / and \ occurrences with the provided
+     * character.
+     *
+     * @param string $string String to find in and replace the character
+     * @param string $inUse String to be a replacement
+     *
+     * @return string
+     *
+     * @see Autoloader::prepareDirectory()
+     * @see Autoloader::prepareNamespace()
+     */
+    protected function prepareString( $string, $inUse ) {
+        $string = (string) $string;
+        $inUse  = (string) $inUse;
+        $string = str_replace( [
+            '\\',
+            '/',
+        ], $inUse, $string );
+        $string = rtrim( $string, $inUse ) . $inUse;
+
+        return $string;
+    }
+
+    /**
+     * This method will prepare directory to a proper format, using
+     * proper character as a directory separator. Of course we can
+     * use / because it works for both Linux and Windows. We do that
+     * because I want everything to looks pretty.
+     *
+     * @param string $directory
+     *
+     * @return string
+     *
+     * @see Autoloader::prepareString()
+     */
+    protected function prepareDirectory( $directory ) {
+        $directory = (string) $directory;
+
+        return $this->prepareString( $directory, DIRECTORY_SEPARATOR );
+    }
+
+    /**
+     * This method will search for the proper class name in the declared
+     * namespaces list. It will check every possible combination until
+     * find proper file (will be loaded) or fail.
+     *
+     * @param string $class
+     *
+     * @return string
+     *
+     * @see Autoloader::$namespaces
+     * @see Autoloader::checkMappedFile()
+     * @see Autoloader::callFile()
+     */
+    public function classLoader( $class ) {
+        $class       = (string) $class;
+        $classPrefix = $class;
+        while ( false !== $position = strrpos( $classPrefix, '\\' ) ) {
+            $classPrefix = substr( $class, 0, $position + 1 );
+            $relClass    = substr( $class, $position + 1 );
+            $mapFile     = $this->checkMappedFile( $classPrefix, $relClass );
+            if ( $mapFile ) {
+                return $mapFile;
+            }
+            $classPrefix = rtrim( $classPrefix, '\\' );
+        }
+
+        return '';
+    }
+
+    /**
+     * This method will check a combination of directories for called
+     * class using callFile. If it find a proper combination, then
+     * a class file will be loaded.
+     *
+     * @param string $namespace
+     * @param string $relClass
+     *
+     * @return string
+     *
+     * @see Autoloader::$namespaces
+     * @see Autoloader::callFile()
+     */
+    protected function checkMappedFile( $namespace, $relClass ) {
+        $namespace = (string) $namespace;
+        $relClass  = (string) $relClass;
+        if ( false === isset( $this->namespaces[ $namespace ] ) ) {
+            return '';
+        }
+        foreach ( $this->namespaces[ $namespace ] as $baseDir ) {
+            $filePath = $baseDir . str_replace( '\\', DIRECTORY_SEPARATOR, $relClass ) . '.php';
+            if ( $this->callFile( $filePath ) ) {
+                return $filePath;
+            }
+        }
+
+        return '';
+    }
+
+    /**
+     * This method will load the class file, if it is readable
+     * and it is not a directory, which is obvious.
+     *
+     * @param string $filePath
+     *
+     * @return bool
+     * @noinspection PhpIncludeInspection
+     */
+    protected function callFile( $filePath ) {
+        $filePath = (string) $filePath;
+        if ( is_readable( $filePath ) && ! is_dir( $filePath ) ) {
+            require_once $filePath;
+
+            return true;
+        }
+
+        return false;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 # PSR Autoloader
-I wrote small PSR-4 Autoloader to use with my projects. Feel free to use it in yours.
-
-###### Warning: This class uses PHP version _7.4_ syntax
+PSR-4 compatible Autoloader.
+I split this project into few classes, to preserve backward compatibility with previous PHP versions. I use 7.4 mostly all of the time, but I realize, that there are many servers with the outdated PHP versions. And since I use this class to my WordPress plugin/theme projects, I decided to use older versions as well.
+You may find desired versions in the proper directories. At this time, you may find four versions, for PHP 5.6, 7.0, 7.1 and 7.4. Of course those versions are upward compatible, so you may use 5.6 version running it with PHP 7.4.
 
 Some example codes:
-
 ```php
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR;
 $theAutoloader = new \Phylax\Autoloader();
 $theAutoloader->registerHandler();
 $theAutoloader->addNamespace( 'Phylax', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' );
 $theAutoloader->addNamespace( 'OtherStuff', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'OtherStuff' );
 # etc.
 ```
-
 Or:
 ```php
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR;
 new \Phylax\Autoloader( 'OtherStuff', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'OtherStuff' );
 ```
 
@@ -25,20 +23,16 @@ For example:
 
 ```php
 if ( ! class_exists( 'Phylax\Autoloader' ) ) {
-	require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'Phylax' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+	require_once __DIR__ . DIRECTORY_SEPARATOR;
 }
 new \Phylax\Autoloader( 'OtherStuff', __DIR__ . DIRECTORY_SEPARATOR . 'Vendors' . DIRECTORY_SEPARATOR . 'OtherStuff' );
 ```
 
-## Authors
-* Łukasz Nowicki <https://lukasznowicki.info/>
-* [Kurs programowania WordPress](https://wpkurs.pl/)
-* [Strony internetowe, aplikacje](https://phylax.pl/)
-
-
+## Author and featured sites:
+* Łukasz Nowicki <https://lukasznowicki.info/> it's my blog, with some WordPress/WooCommerce tips
+* [Kurs programowania WordPress](https://wpkurs.pl/) it's my WordPress developing course, in polish
+* [Strony internetowe, aplikacje](https://phylax.pl/) and this is my company's page.
 
 ## License
-Copyright 2016 phylax.pl Łukasz Nowicki
-<https://phylax.pl/>
-
+Copyright 2016-2020 phylax.pl Łukasz Nowicki <https://phylax.pl/>
 Licensed under the GPLv2 or later: <http://www.gnu.org/licenses/gpl-2.0.html>
